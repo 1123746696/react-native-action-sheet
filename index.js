a170439ed885b3d34df6b5a0fbb07ac92b6915d8
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2018-05-07 15:27:13 +0800

    change index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*
-* (The MIT License)
-* Copyright (c) 2015-2016 YunJiang.Fang <42550564@qq.com>
-* @providesModule ActionSheet
-* @flow-weak
-*/
+ * (The MIT License)
+ * Copyright (c) 2015-2016 YunJiang.Fang <42550564@qq.com>
+ * @providesModule ActionSheet
+ * @flow-weak
+ */
 'use strict';
 
 
@@ -17,46 +17,43 @@ import {
 import Button from './button'
 import Overlay from './overlay'
 import Sheet from './sheet'
-
-module.exports =  React.createClass({
-    getDefaultProps: function() {
-        return {
-            cancelText: 'Cancel',
-        };
-    },
+export default class Action extends Component{
+    static Button = Button
+    static propTypes ={
+    cancelText: 'Cancel',
+    }
     render() {
         return (
-            <Overlay visible={this.props.visible}>
+                <Overlay visible={this.props.visible}>
                 <View style={styles.actionSheetContainer}>
-                    <TouchableOpacity
-                        style={{flex:1}}
-                        onPress={this.props.onCancel}>
-                    </TouchableOpacity>
-                    <Sheet visible={this.props.visible}>
-                        <View style={styles.buttonContainer}>
-                            {this.props.children}
-                        </View>
-                        <Button
-                            buttonStyle={{marginTop:10, borderRadius:7}}
-                            textStyle={{color:'#007cf9'}}
-                            onPress={this.props.onCancel}>{this.props.cancelText}</Button>
-                    </Sheet>
+                <TouchableOpacity
+                style={{flex:1}}
+                onPress={this.props.onCancel}>
+                </TouchableOpacity>
+                <Sheet visible={this.props.visible}>
+                <View style={styles.buttonContainer}>
+                {this.props.children}
                 </View>
-            </Overlay>
-        );
-    },
-});
-module.exports.Button = Button;
+                <Button
+                buttonStyle={{marginTop:10, borderRadius:7}}
+                textStyle={{color:'#007cf9'}}
+                onPress={this.props.onCancel}>{this.props.cancelText}</Button>
+                </Sheet>
+                </View>
+                </Overlay>
+                );
+    }
+}
 
 var styles = StyleSheet.create({
-    actionSheetContainer: {
-        flex: 1,
-        padding: 10,
-        justifyContent: "flex-end",
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    buttonContainer: {
-        borderRadius:7,
-        overflow: 'hidden',
-    }
-});
+                               actionSheetContainer: {
+                               flex: 1,
+                               padding: 10,
+                               justifyContent: "flex-end",
+                               backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                               },
+                               buttonContainer: {
+                               borderRadius:7,
+                               overflow: 'hidden',
+                               }
+                               });
